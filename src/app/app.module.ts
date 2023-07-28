92c98287d84880bacec8e7d8b3e27478fb812030
David Yepes <djvash01@gmail.com>
2023-07-28 12:01:42 -0500

    feat: 🎸 create component header and map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchToolbarComponent } from './search-toolbar/search-toolbar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchToolbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-toolbar/search-toolbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-toolbar',
+  templateUrl: './search-toolbar.component.html',
+  styleUrls: ['./search-toolbar.component.scss']
+})
+export class SearchToolbarComponent {
+
+}
